a211c47c9317b0ed5fb90cdc2151f327230eb076
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2022-11-17 17:56:37 +0300

    Initial commit

---
+++ 1/Main.java
@@ -0,0 +1,16 @@
+// package com.company;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+    int a = -10;
+    String str1 = "qwert";
+    Scanner sc = new Scanner(System.in);
+    String str2 = sc.nextLine();
+    for (int i=0; i<=10; i++){
+      System.out.println(i);
+    }
+    double b = a / 7.0;
+    System.out.println(b);
+  }
+}
